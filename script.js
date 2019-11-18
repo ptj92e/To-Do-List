b7a1bb0563fa95155459d7587d7d004ebaf53a8c
Phillip Jones <phillipjones@Amandas-MacBook-Pro.local>
2019-11-18 14:00:54 -0600

    Added variables

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 //Creating Variables
+let taNine = $("#textNine");
+let taTen = $("#textTen");
+let taEleven = $("#textEleven");
+let taTwelve = $("#textTwelve");
+let taOne = $("#textOne");
+let taTwo = $("#textTwo");
+let taThree = $("#textThree");
+let taFour = $("#textFour");
+let taFive = $("#textFive");
+
 let currentDate = moment().format("dddd, MMMM DD");
 
 //Object for saving text
@@ -31,5 +41,5 @@ $("#currentDay").text(currentDate);
 //Setting up a function to save the content to the object. 
 function saveContent() {
     event.preventDefault(),
-    
+
 }
