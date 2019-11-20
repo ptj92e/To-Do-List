c6e12431e5c10c3cf81273fdcb3f60fded5c9845
Phillip Jones <phillipjones@Amandas-MacBook-Pro.local>
2019-11-20 10:33:20 -0600

    Updated function

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ let currentDate = moment().format("dddd MMMM D");
 let dayText = $("#currentDay");
 dayText.text(currentDate);
 
-//Set up an on click event once that recognizes each button.
-//Each time a button is clicked, it pulls the id from the row and the text from the text area and puts them into variables. 
-//It then pairs them into a key and a value needed to push and pull from local storage.
+//Set up an on click event once that recognizes each button. Each time a button is clicked, it pulls the id from the row and the text from the text area and puts them into variables. It then pairs them into a key and a value needed to push and pull from local storage.
 $(".saveBtn").on("click", saveContent); 
 
 function saveContent() {
@@ -14,8 +12,7 @@ function saveContent() {
     localStorage.setItem(hour, task);
 };
 
-//Set up variables for text areas.
-//This line is reaching down through the divs to access the child text areas inside. This will allow for attribute and text changes.
+//Set up variables for text areas. This line is reaching down through the divs to access the child text areas inside. This will allow for attribute and text changes.
 let textNine = $("#9am").children(".row").children(".input-group").children("textarea");
 let textTen = $("#10am").children(".row").children(".input-group").children("textarea");
 let textEleven = $("#11am").children(".row").children(".input-group").children("textarea");
@@ -26,8 +23,7 @@ let textThree = $("#3pm").children(".row").children(".input-group").children("te
 let textFour = $("#4pm").children(".row").children(".input-group").children("textarea");
 let textFive = $("#5pm").children(".row").children(".input-group").children("textarea");
 
-//Adding the text from local storage to the text area.
-//This allows the page to keep the text when refreshed.
+//Adding the text from local storage to the text area. This allows the page to keep the text when refreshed.
 textNine.val(localStorage.getItem("9am"));
 textTen.val(localStorage.getItem("10am"));
 textEleven.val(localStorage.getItem("11am"));
@@ -40,7 +36,16 @@ textFive.val(localStorage.getItem("5pm"));
 
 //This function changes the background color of the text areas depending on different parameters. If it is before the time on the time block, the background is green. If it is the same time as the time block, the background is red. If it is the time after the time block, the background is grey. 
 function backgroundChange() {
-        
+
+
+//This if statement determines what time it is compared the variables set above. 
+    if (< 0) {
+        .addCLass("future");
+    } else if (= 0) {
+        .addClass("present");
+    } else {
+        .addClass("past")
+    }
 }
 
 backgroundChange();
