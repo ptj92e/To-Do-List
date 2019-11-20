4ceeda3ef6ef126b443590e9cff906a8ab772980
Phillip Jones <phillipjones@Amandas-MacBook-Pro.local>
2019-11-20 08:14:03 -0600

    Updated pseudo code

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-//Setting current date
+//This block of code is pulling time information from Moment.js and adding it to the page to update whenever the day changes. 
 let currentDate = moment().format("dddd MMMM D");
 let dayText = $("#currentDay");
 dayText.text(currentDate);
 
 //Set up an on click event once that recognizes each button.
+//Each time a button is clicked, it pulls the id from the row and the text from the text area and puts them into variables. 
+//It then pairs them into a key and a value needed to push and pull from local storage.
 $(".saveBtn").on("click", saveContent); 
 
 function saveContent() {
     let hour = $(this).parent().parent().parent().attr("id");
     let task = $(this).siblings("textarea").val();
-
     localStorage.setItem(hour, task);
-    console.log(localStorage);
 };
 
-//Set up variables for text areas
+//Set up variables for text areas.
+//This line is reaching down through the divs to access the child text areas inside. This will allow for attribute and text changes.
 let textNine = $("#9am").children(".row").children(".input-group").children("textarea");
 let textTen = $("#10am").children(".row").children(".input-group").children("textarea");
 let textEleven = $("#11am").children(".row").children(".input-group").children("textarea");
@@ -25,6 +26,8 @@ let textThree = $("#3pm").children(".row").children(".input-group").children("te
 let textFour = $("#4pm").children(".row").children(".input-group").children("textarea");
 let textFive = $("#5pm").children(".row").children(".input-group").children("textarea");
 
+//Adding the text from local storage to the text area.
+//This allows the page to keep the text when refreshed.
 textNine.val(localStorage.getItem("9am"));
 textTen.val(localStorage.getItem("10am"));
 textEleven.val(localStorage.getItem("11am"));
@@ -34,3 +37,4 @@ textTwo.val(localStorage.getItem("2pm"));
 textThree.val(localStorage.getItem("3pm"));
 textFour.val(localStorage.getItem("4pm"));
 textFive.val(localStorage.getItem("5pm"));
+
