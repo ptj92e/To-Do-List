e8aef23aad66fd2f66998916f22be0a74e782575
Phillip Jones <phillipjones@Amandas-MBP.attlocal.net>
2019-11-20 21:53:43 -0600

    Updated Military Time and variables

--- script.js
+++ script.js
@@ -13,40 +13,45 @@ function saveContent() {
 };
 
 //Set up variables for text areas. This line is reaching down through the divs to access the child text areas inside. This will allow for attribute and text changes.
-let textNine = $("#9am").children(".row").children(".input-group").children("textarea");
-let textTen = $("#10am").children(".row").children(".input-group").children("textarea");
-let textEleven = $("#11am").children(".row").children(".input-group").children("textarea");
-let textTwelve = $("#12pm").children(".row").children(".input-group").children("textarea");
-let textOne = $("#1pm").children(".row").children(".input-group").children("textarea");
-let textTwo = $("#2pm").children(".row").children(".input-group").children("textarea");
-let textThree = $("#3pm").children(".row").children(".input-group").children("textarea");
-let textFour = $("#4pm").children(".row").children(".input-group").children("textarea");
-let textFive = $("#5pm").children(".row").children(".input-group").children("textarea");
+let textNine = $("#9").children(".row").children(".input-group").children("textarea");
+let textTen = $("#10").children(".row").children(".input-group").children("textarea");
+let textEleven = $("#11").children(".row").children(".input-group").children("textarea");
+let textTwelve = $("#12").children(".row").children(".input-group").children("textarea");
+let textOne = $("#13").children(".row").children(".input-group").children("textarea");
+let textTwo = $("#14").children(".row").children(".input-group").children("textarea");
+let textThree = $("#15").children(".row").children(".input-group").children("textarea");
+let textFour = $("#16").children(".row").children(".input-group").children("textarea");
+let textFive = $("#17").children(".row").children(".input-group").children("textarea");
 
 //Adding the text from local storage to the text area. This allows the page to keep the text when refreshed.
-textNine.val(localStorage.getItem("9am"));
-textTen.val(localStorage.getItem("10am"));
-textEleven.val(localStorage.getItem("11am"));
-textTwelve.val(localStorage.getItem("12pm"));
-textOne.val(localStorage.getItem("1pm"));
-textTwo.val(localStorage.getItem("2pm"));
-textThree.val(localStorage.getItem("3pm"));
-textFour.val(localStorage.getItem("4pm"));
-textFive.val(localStorage.getItem("5pm"));
+textNine.val(localStorage.getItem("9"));
+textTen.val(localStorage.getItem("10"));
+textEleven.val(localStorage.getItem("11"));
+textTwelve.val(localStorage.getItem("12"));
+textOne.val(localStorage.getItem("13"));
+textTwo.val(localStorage.getItem("14"));
+textThree.val(localStorage.getItem("15"));
+textFour.val(localStorage.getItem("16"));
+textFive.val(localStorage.getItem("17"));
 
 //This function changes the background color of the text areas depending on different parameters. If it is before the time on the time block, the background is green. If it is the same time as the time block, the background is red. If it is the time after the time block, the background is grey. 
 function backgroundChange() {
-    let i = 2;
+    let currentHour = moment().hour();
 
-//This if statement determines what time it is compared the variables set above. 
-    if (i > 0) {
-        textTwo.addCLass("past");
-    } else if (i === 0) {
-        textTwo.addClass("present");
-    } else {
-        textTwo.addClass("future")
-    }
+    $("#time-block").each(function() {
+        if ($("id") > currentHour) {
+            $("#textarea").addClass("future");
+            console.log("yes");
+        } else if (parseInt($("id")) === currentHour) {
+            $("#textarea").addClass("present");
+            console.log("maybe");
+        } else {
+            $("#textarea").addClass("past");
+            console.log("no");
+        }
+    });  
 }
 
-//This is calling the function to actually change the backgrounds based off of the time. 
+// This is calling the function to actually change the backgrounds based off of the time. 
 backgroundChange();
+
